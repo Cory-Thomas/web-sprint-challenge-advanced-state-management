e6e74b2a2eda8dc0a4483e7cf029824df5443941
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-09-11 12:02:47 -0400

    Got the data from the server to display on the screen with redux

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,34 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchSmurfs } from ".././store/actions";
 import "./App.css";
-class App extends Component {
-  render() {
+import Smurfs from './Smurfs';
+
+const App = ({ fetchSmurfs, loadingSmurfs, errorMessage }) => {
+    useEffect( () => {
+        fetchSmurfs();
+      }, [ fetchSmurfs ]
+    );
+   
     return (
       <div className="App">
         <h1>SMURFS! W/Redux</h1>
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        {/* <SmurfForm /> */}
+        { !loadingSmurfs ? <Smurfs /> : <div>... Fetching Smurf Village</div> }
+        { errorMessage !== "" ? <div>{ errorMessage }</div> : null }
+       
       </div>
     );
-  }
-}
+};
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    loadingSmurfs: state.loadingSmurfs,
+    errorMessage: state.errorMessage
+  };
+};
+
+export default connect( mapStateToProps, { fetchSmurfs })( App );
---
+++ smurfs/src/store/actions/smurfActions.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export const FETCH_SMURFS = "FETCH_SMURFS";
+export const FETCH_SMURFS_SUCCESS = "FETCH_SMURFS_SUCCESS";
+export const FETCH_SMURFS_ERROR = "FETCH_SMURFS_ERROR";
+
+export const fetchSmurfs = () => {
+  return ( dispatch ) => {
+    dispatch({ type: FETCH_SMURFS });
+    axios
+      .get( "http://localhost:3333/smurfs" )
+        .then( response => {
+            console.log( response.data );
+            dispatch({ type: FETCH_SMURFS_SUCCESS, payload: response.data })
+        })
+        .catch( error => {
+            console.log( error );
+            dispatch({
+                type: FETCH_SMURFS_ERROR,
+                payload: { message: "Smurf Village did not load" }
+         });
+    });
+    };
+};
